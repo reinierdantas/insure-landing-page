b78248add5add43ab72c5f0f8dfe58a406122455
Reinier Dantas <50921411+reinierdantas@users.noreply.github.com>
2020-01-22 16:33:08 -0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,7 +1,10 @@
+// Toogle menu when click on icon
 $('.mob-logo').click(function(){
     $('.mobile-nav').toggleClass('active');
 });
 
+
+//Toogle buttons open/close
 $('.mob-logo').click(function(){
     $('.mobilelogo-close').toggleClass('activate');
 });
